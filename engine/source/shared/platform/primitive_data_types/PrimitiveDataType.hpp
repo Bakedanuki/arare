6fcf35a7468c0d7e137f90682d92d645b5c3ca7a
Bakedanuki <48862619+Bakedanuki@users.noreply.github.com>
2021-02-14 15:07:07 +0900

    Create primitive data types

---
+++ engine/source/shared/platform/primitive_data_types/PrimitiveDataType.hpp
@@ -0,0 +1,31 @@
+#ifndef ARARE_PRIMITIVEDATATYPE_HPP
+#define ARARE_PRIMITIVEDATATYPE_HPP
+
+namespace arare {
+
+// void type
+using Void = void;
+
+// unsigned integer types
+using UInt8 = unsigned char;
+using UInt32 = unsigned int;
+using UInt64 = unsigned long long;
+
+// signed integer types
+using Int8 = signed char;
+using Int32 = signed int;
+using Int64 = signed long long;
+
+// floating point types
+using Float32 = float;
+using Float64 = double;
+
+// character types
+using Char = char;
+
+// boolean type
+using Bool = Int32;
+
+}  // namespace arare
+
+#endif
---
+++ engine/source/shared/platform/primitive_data_types/PrimitiveDataValue.hpp
@@ -0,0 +1,11 @@
+#ifndef ARARE_PRIMITIVEDATAVALUE_HPP
+#define ARARE_PRIMITIVEDATAVALUE_HPP
+
+// void type value
+#define ARARE_NULL nullptr
+
+// boolean type value
+#define ARARE_FALSE 0
+#define ARARE_TRUE 1
+
+#endif
